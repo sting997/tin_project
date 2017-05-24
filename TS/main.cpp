149592ca99244051acc336ccbe71b6203a618c82
riuoku <riuoku@hotmail.com>
2017-05-24 14:38:47 +0200

    Added return IP feature on TS server using select

--- TS/main.cpp
+++ TS/main.cpp
@@ -7,18 +7,49 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <cstdlib>
+#include <arpa/inet.h>
+#include <errno.h>
 #include <unistd.h>
+
+void returnIP( int sock ) {
+    int n;
+    socklen_t len;
+    char buf[1024];
+    struct sockaddr_in remote;
+
+    /* need to know how big address struct is, len must be set before the
+       call to recvfrom!!! */
+
+    len = sizeof(remote);
+
+    /* read a datagram from the socket (put result in bufin) */
+    n=recvfrom(sock,buf,1024,0,(struct sockaddr *) &remote, &len);
+
+    /* print out the address of the sender */
+    printf("Got a datagram from %s port %d\n",
+           inet_ntoa(remote.sin_addr), ntohs(remote.sin_port));
+
+    if (n<0) {
+        perror("Error receiving data");
+    } else {
+        printf("GOT %d BYTES\n",n);
+        /* Got something, just send it back */
+        sendto(sock,buf,n,0,(struct sockaddr *) &remote, len);
+    }
+}
 
 
 int main()
 {
-    int sock, length;
+    int udpfd, nready, maxfdp1;
+    socklen_t len;
+    fd_set rset;
     struct sockaddr_in name;
     char buf[1024];
 
-    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    udpfd = socket(AF_INET, SOCK_DGRAM, 0);
 
-    if (sock == -1) {
+    if (udpfd == -1) {
         perror("opening datagram socket");
         exit(1);
     }
@@ -28,15 +59,15 @@ int main()
     name.sin_addr.s_addr = INADDR_ANY;
     name.sin_port = htons(9000);
 
-    if (bind(sock,(struct sockaddr *)&name, sizeof name) == -1) {
+    if (bind(udpfd,(struct sockaddr *)&name, sizeof name) == -1) {
         perror("binding datagram socket");
         exit(1);
     }
 
     /* Wydrukuj na konsoli numer portu */
-    length = sizeof(name);
+    len = sizeof(name);
 
-    if (getsockname(sock,(struct sockaddr *) &name, (socklen_t *) &length)
+    if (getsockname(udpfd,(struct sockaddr *) &name, &len)
         == -1) {
         perror("getting socket name");
         exit(1);
@@ -44,10 +75,21 @@ int main()
 
     printf("Socket port #%d\n", ntohs(name.sin_port));
 
-    /* Read from the socket. */
-    if ( read(sock, buf, 1024) == -1 )
-        perror("receiving datagram packet");
-    printf("-->%s\n", buf);
-    close(sock);
+    FD_ZERO(&rset);
+    maxfdp1 = udpfd + 1;
+
+    while(1) {
+        FD_SET(udpfd, &rset);
+        if ( (nready = select(maxfdp1, &rset, NULL, NULL, NULL)) < 0) {
+            if (errno == EINTR)
+                continue;
+            else
+                perror("Some shit happened with select");
+        }
+
+        if (FD_ISSET(udpfd, &rset)) {
+            returnIP(udpfd);
+        }
+    }
     exit(0);
 }

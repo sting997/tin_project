c610e7eb1b6caaef67dd7791a80b77692b69a4c4
monspid <monspid@gmail.com>
2017-05-25 23:38:47 +0200

    Sample of PrivilegeManager

--- TS/PrivilegeManager.cpp
+++ TS/PrivilegeManager.cpp
@@ -3,3 +3,56 @@
 //
 
 #include "PrivilegeManager.h"
+
+char* PrivilegeManager::getPrivilegeInfo(char* auth_data) {
+    char* privilege_info = nullptr;
+    std::vector<char *> split_auth_data = getSplitAuthData(auth_data);
+    if(isValidAuthData(split_auth_data)) {
+        // get it from DB
+        privilege_info = (char *) "Ok";
+    }
+
+    return privilege_info;
+}
+
+bool PrivilegeManager::isValidAuthData(std::vector<char *> split_auth_data) {
+    return split_auth_data.size() == 5 and isValidIP(split_auth_data) and
+            isValidServerName(split_auth_data) and isValidServiceName(split_auth_data) and
+            isValidUsername(split_auth_data) and isValidPassword(split_auth_data);
+}
+
+std::vector<char *> PrivilegeManager::getSplitAuthData(char auth_data[]) {
+    std::vector<char *> split_auth_data;
+
+    char * pch = strtok(auth_data, ";");
+
+    while (pch != NULL) {
+        split_auth_data.push_back(pch);
+        pch = strtok (NULL, ";");
+    }
+
+    return split_auth_data;
+}
+
+bool PrivilegeManager::isValidIP(std::vector<char *> split_auth_data) {
+    return split_auth_data[0] != "";
+}
+
+bool PrivilegeManager::isValidServerName(std::vector<char *> split_auth_data) {
+    return split_auth_data[1] != "";
+}
+
+bool PrivilegeManager::isValidServiceName(std::vector<char *> split_auth_data) {
+    return split_auth_data[2] != "";
+}
+
+bool PrivilegeManager::isValidUsername(std::vector<char *> split_auth_data) {
+    return split_auth_data[3] != "";
+}
+
+bool PrivilegeManager::isValidPassword(std::vector<char *> split_auth_data) {
+    return split_auth_data[4] != "";
+}
+
+
+
--- TS/PrivilegeManager.h
+++ TS/PrivilegeManager.h
@@ -4,11 +4,22 @@
 
 #ifndef TIN_PRIVILEGEMANAGER_H
 #define TIN_PRIVILEGEMANAGER_H
-
+#include <cstring>
+#include <vector>
 
 class PrivilegeManager {
+    std::vector<char *> getSplitAuthData(char auth_data[]);
+    bool isValidAuthData(std::vector<char *> split_auth_data);
+    bool isValidIP(std::vector<char *> split_auth_data);
+    bool isValidServerName(std::vector<char *> split_auth_data);
+    bool isValidServiceName(std::vector<char *> split_auth_data);
+    bool isValidUsername(std::vector<char *> split_auth_data);
+    bool isValidPassword(std::vector<char *> split_auth_data);
 
+public:
+    char* getPrivilegeInfo(char* auth_data);
 };
+
 
 
 #endif //TIN_PRIVILEGEMANAGER_H

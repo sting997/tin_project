eb6537e9ac8f6a15aa62037673604af9ca42951d
monspid <monspid@gmail.com>
2017-05-27 23:08:44 +0200

    implemented RequestManager in TS.

--- TS/main.cpp
+++ TS/main.cpp
@@ -15,47 +15,45 @@ void prepareSocket(int &fd, int domain, int type, int protocol, struct sockaddr_
 void fillSockaddr_in(struct sockaddr_in &name, sa_family_t sin_family, in_addr_t s_addr, unsigned short sin_port);
 
 int main() {
-    int udpfd, udpfd2, nready, maxfdp1;
+    int udpfd_IP, udpfd_ticket, nready, maxfdp;
     fd_set rset;
     struct sockaddr_in name;
-    char buf[1024];
 
     fillSockaddr_in(name, AF_INET, INADDR_ANY, 9000);
-    prepareSocket(udpfd, AF_INET, SOCK_DGRAM, 0, name);
+    prepareSocket(udpfd_IP, AF_INET, SOCK_DGRAM, 0, name);
     fillSockaddr_in(name, AF_INET, INADDR_ANY, 8000);
-    prepareSocket(udpfd2, AF_INET, SOCK_DGRAM, 0, name);
+    prepareSocket(udpfd_ticket, AF_INET, SOCK_DGRAM, 0, name);
 
     FD_ZERO(&rset);
-    maxfdp1 = udpfd2 > udpfd ? udpfd2 + 1 : udpfd + 1;
-    while (true) {
-        FD_SET(udpfd, &rset);
-        FD_SET(udpfd2, &rset);
-        if ((nready = select(maxfdp1, &rset, NULL, NULL, NULL)) < 0)
-            perror("Something bad happened with select");
+    maxfdp = udpfd_ticket > udpfd_IP ? udpfd_ticket + 1 : udpfd_IP + 1;
 
-        if (FD_ISSET(udpfd, &rset)) {
-            RequestManager requestManager = RequestManager(udpfd);
-            requestManager.requestIP();
+    while (true) {
+        FD_SET(udpfd_IP, &rset);
+        FD_SET(udpfd_ticket, &rset);
+
+        if ((nready = select(maxfdp, &rset, NULL, NULL, NULL)) < 0) {
+            perror("Something bad happened with select");
+            return nready;
         }
 
-        else if (FD_ISSET(udpfd2, &rset)) {
-            RequestManager requestManager = RequestManager(udpfd2);
+        if (FD_ISSET(udpfd_IP, &rset)) {
+            RequestManager requestManager = RequestManager(udpfd_IP);
+            requestManager.requestIP();
+        } else if (FD_ISSET(udpfd_ticket, &rset)) {
+            RequestManager requestManager = RequestManager(udpfd_ticket);
             requestManager.requestTicket();
         }
     }
-
-    exit(0);
 }
 
 void fillSockaddr_in(struct sockaddr_in &name, sa_family_t sin_family, in_addr_t s_addr, unsigned short sin_port) {
-    /* Create name with wildcards. */
     name.sin_family = sin_family;
     name.sin_addr.s_addr = s_addr;
     name.sin_port = htons(sin_port);
 }
 
 void prepareSocket(int &fd, int domain, int type, int protocol, struct sockaddr_in name) {
-    socklen_t len;
+    socklen_t len = sizeof(name);
     fd = socket(domain, type, protocol);
 
     if (fd == -1) {
@@ -63,13 +61,10 @@ void prepareSocket(int &fd, int domain, int type, int protocol, struct sockaddr_
         exit(1);
     }
 
-    if (bind(fd, (struct sockaddr *) &name, sizeof name) == -1) {
+    if (bind(fd, (struct sockaddr *) &name, len) == -1) {
         perror("binding datagram socket");
         exit(1);
     }
-
-    //print port number on console
-    len = sizeof(name);
 
     if (getsockname(fd, (struct sockaddr *) &name, &len) == -1) {
         perror("getting socket name");

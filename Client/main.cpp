d1f9f09f2cd28630c55367059a1730611997c737
riuoku <riuoku@hotmail.com>
2017-05-28 14:42:44 +0200

    Implemented TCP on SN stuff

--- Client/main.cpp
+++ Client/main.cpp
@@ -15,11 +15,18 @@
 #define PORT 9000
 #define UDP_ECHO_PORT 6000
 #define UDP_TIME_PORT 6001
+#define TCP_ECHO_PORT 5000
+#define TCP_TIME_PORT 5001
 
 
 //TODO
 //refactor this code, divide into functions
-void communicationTest(int port);
+int checkIfEnd(char const* buf, char const* seq);
+
+void udpTest(int port);
+void tcpEchoTest();
+void tcpTimeTest();
+int startTcpCon(char const* ip, int port);
 
 int main(int argc, char *argv[]) {
     int sock;
@@ -101,21 +108,25 @@ int main(int argc, char *argv[]) {
 			printf("Received roaming package, didn't want it though!\n");
 	}	
 
-    communicationTest(UDP_TIME_PORT);
-    communicationTest(UDP_ECHO_PORT);
+    udpTest(UDP_TIME_PORT);
+    udpTest(UDP_ECHO_PORT);
+
+    tcpTimeTest();
+    tcpEchoTest();
+
 
     close(sock);
     exit(0);
 }
 
-void communicationTest(int port) {
+void udpTest(int port) {
     int sock;
     char buf[1024];
     struct sockaddr_in remote;
     struct sockaddr_in service;
 
     /* Create socket on which to send. */
-    sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    sock = socket(PF_INET, SOCK_DGRAM, 0);
 
     if (sock == -1) {
         perror("opening datagram socket");
@@ -128,8 +139,7 @@ void communicationTest(int port) {
     service.sin_port = htons(port);
 
     bzero(buf, 1024);
-    memcpy(buf + 1, "Echo: Lorem Ipsum", sizeof("Echo: Lorem Ipsum"));
-
+    memcpy(buf + 1, "UDPEcho: Lorem Ipsum", sizeof("UDPEcho: Lorem Ipsum"));
     if (sendto(sock, buf, sizeof buf, 0, (struct sockaddr *) &service, sizeof service) == -1)
         perror("sending datagram message");
     //receive package from server
@@ -137,21 +147,126 @@ void communicationTest(int port) {
     int n;
 
     n = recvfrom(sock, buf, 1024, 0, (struct sockaddr *) &remote, &len);
+
     if (n < 0)
         perror("Error receiving data");
     else {
         if (buf[0] == SERVICE_GRANTED) {
             printf("Received package from service server: %s\n", inet_ntoa(remote.sin_addr));
-            if (port == UDP_TIME_PORT)
+            if (port == UDP_TIME_PORT) {
+                printf("%d", strlen(buf));
                 printf("%s\n", std::asctime(std::localtime(reinterpret_cast<time_t *>(buf + 1))));
+            }
             else
                 printf("%s\n", (buf + 1));
-        } else if (buf[0] == SERVICE_REFUSED) {
+        }
+        else if (buf[0] == SERVICE_REFUSED) {
             printf("Received package from service server: %s\n", inet_ntoa(remote.sin_addr));
             printf("%s\n", (buf + 1));
-        } else
+        }
+        else
             printf("Received roaming package, didn't want it though!\n");
     }
 
     close(sock);
+}
+
+void tcpEchoTest() {
+    int sock;
+    sock = startTcpCon("127.0.0.1", TCP_ECHO_PORT);
+    char buf[1024];
+    int rval, endpos;
+
+    bzero(buf, 1024);
+
+    //keep communicating with server
+    while(true) {
+        printf("To ECHO: ");
+        fgets(buf, 1024, stdin);
+
+        //Send some data
+        if(send(sock , buf , strlen(buf) , 0) < 0) {
+            perror("Send failed:");
+            exit(1);
+        }
+        endpos= checkIfEnd(buf, "END");
+        if(endpos != std::string::npos)
+            break;
+    }
+    while(true) {
+        if((rval = read(sock , buf , 1024)) == -1) {
+            perror("Read failed:");
+            exit(1);
+        }
+        if(rval == 0) {
+            printf("Ending connection.\n");
+            break;
+        }
+        else
+            printf("%s\n",buf);
+    }
+    close(sock);
+}
+
+void tcpTimeTest() {
+    int sock;
+    sock = startTcpCon("127.0.0.1", TCP_TIME_PORT);
+    char buf[1024];
+    int rval;
+
+    bzero(buf, 1024);
+
+    memcpy(buf, "TCP TIME PLS", sizeof("TCP TIME PLS"));
+
+    if(send(sock , buf , strlen(buf) , 0) < 0) {
+        perror("Send failed:");
+        exit(1);
+    }
+
+    bzero(buf, 1024);
+    if((rval = read(sock , buf , 1024)) == -1) {
+        perror("Read failed:");
+        exit(1);
+    }
+    if(rval == 0) {
+        printf("Service server disconnected.\n");
+    }
+    else {
+        fprintf(stderr, "%d", strlen(buf));
+        printf("%s\n", std::asctime(std::localtime(reinterpret_cast<time_t *>(buf + 1))));
+    }
+    close(sock);
+}
+
+int startTcpCon(char const* ip, int port) {
+    int sock;
+    struct sockaddr_in service;
+
+    /* Create socket on which to send. */
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+
+    if (sock == -1) {
+        perror("Opening datagram socket:");
+        exit(1);
+    }
+
+    memset(&service, 0, sizeof(service));
+    service.sin_family = AF_INET;
+    service.sin_addr.s_addr = inet_addr(ip);
+    service.sin_port = htons(port);
+
+    //Connect to remote server
+    if (connect(sock , (struct sockaddr *)&service , sizeof(service)) < 0) {
+        perror("Connect failed:");
+        return -1;
+    }
+    return sock;
+}
+
+int checkIfEnd(char const* buf, char const* seq) {
+    std::string bufs(buf);
+    std::string subs(seq);
+
+    auto pos = bufs.find(seq);
+    return pos;
 }
--- SN/main.cpp
+++ SN/main.cpp
@@ -12,25 +12,32 @@
 #include <cstring>
 #include <algorithm>
 #include <ctime>
+#include <unistd.h>
+#include <string>
 #include "../protocol_codes.h"
 #include "TicketCorrectnessTester.h"
 
 #define LISTENQ 5
-#define MAXLINE 1024
+#define BUFFER_SIZE 1024
 
 void prepareSocket(int &fd, int domain, int type, int protocol, struct sockaddr_in name);
 void fillSockaddr_in(struct sockaddr_in &name, short sin_family, unsigned long s_addr, unsigned short sin_port);
 
-int maxFd1(int tcpfd, int tcpfd2, int udpfd, int udpfd2);
-void UDPEcho(int &udpfd, char* buf);
-void UDPTime(int &udpfd, char* buf);
+void prepareRefuseBuffer(char * buf, int errNum);
+int checkIfEnd(char const* buf, char const* seq);
+
+int maxFd1(int fd, int fd2, int fd3, int fd4);
+void UDPEcho(int fd, char* buf);
+void UDPTime(int fd, char* buf);
+void TCPEcho(int fd, char* buf);
+void TCPTime(int fd, char* buf);
 
 int main()
 {
     int tcpfd, tcpfd2, udpfd, udpfd2, nready, maxfdp1;
     fd_set rset;
     struct sockaddr_in servaddr;
-    char buf[1024];
+    char buf[BUFFER_SIZE];
 
     fillSockaddr_in(servaddr, AF_INET, INADDR_ANY, 5000);
     prepareSocket(tcpfd, AF_INET, SOCK_STREAM, 0, servaddr);
@@ -42,23 +49,24 @@ int main()
     fillSockaddr_in(servaddr, AF_INET, INADDR_ANY, 6001);
     prepareSocket(udpfd2, AF_INET, SOCK_DGRAM, 0, servaddr);
 
-    FD_ZERO(&rset);
     maxfdp1 = maxFd1(tcpfd, tcpfd2, udpfd, udpfd2);
-
     while(true) {
+        FD_ZERO(&rset);
         FD_SET(tcpfd, &rset);
         FD_SET(tcpfd2, &rset);
         FD_SET(udpfd, &rset);
         FD_SET(udpfd2, &rset);
 
-        if ((nready = select(maxfdp1, &rset, NULL, NULL, NULL)) < 0)
+        if ((nready = select(maxfdp1, &rset, NULL, NULL, NULL)) < 0) {
             perror("Something bad happened with select");
+            exit(0);
+        }
 
-        //if (FD_ISSET(tcpfd, &rset))
-            //TCPEcho(udpfd2);
+        if (FD_ISSET(tcpfd, &rset))
+            TCPEcho(tcpfd, &buf[0]);
 
-        //if (FD_ISSET(tcpfd2, &rset))
-            //TCPTime(udpfd);
+        if (FD_ISSET(tcpfd2, &rset))
+            TCPTime(tcpfd2, &buf[0]);
 
         if (FD_ISSET(udpfd, &rset))
             UDPEcho(udpfd, &buf[0]);
@@ -66,8 +74,6 @@ int main()
         if (FD_ISSET(udpfd2, &rset))
             UDPTime(udpfd2, &buf[0]);
     }
-
-    exit(0);
 }
 
 void fillSockaddr_in(struct sockaddr_in &name, short sin_family, unsigned long s_addr, unsigned short sin_port){
@@ -91,89 +97,194 @@ void prepareSocket(int &fd, int domain, int type, int protocol, struct sockaddr_
         exit(1);
     }
 
-    if (protocol == SOCK_STREAM && (listen(fd, LISTENQ) == -1)) {
+    if (type == SOCK_STREAM && (listen(fd, LISTENQ) == -1)) {
         perror("listen on socket");
         exit(1);
     }
 }
 
-int maxFd1(int tcpfd, int tcpfd2, int udpfd, int udpfd2) {
-    int max = tcpfd;
+int maxFd1(int fd, int fd2, int fd3, int fd4) {
+    int max = fd;
 
-    if (max < tcpfd2)
-        max = tcpfd2;
+    if (max < fd2)
+        max = fd2;
 
-    if (max < udpfd)
-        max = udpfd;
+    if (max < fd3)
+        max = fd3;
 
-    if (max < udpfd2)
-        max = udpfd2;
+    if (max < fd4)
+        max = fd4;
 
     return max + 1;
 }
 
-void UDPEcho(int &udpfd, char* buf) {
+void UDPEcho(int fd, char* buf) {
     int n, k;
     struct sockaddr_in cliaddr;
     socklen_t len = sizeof(cliaddr);
-
-    n = recvfrom(udpfd, buf, MAXLINE, 0,(struct sockaddr *) &cliaddr, &len);
+    n = recvfrom(fd, buf, BUFFER_SIZE, 0,(struct sockaddr *) &cliaddr, &len);
 
     if((k = TicketCorrectnessTester::CheckTicket(buf)) == 0) {
         buf[0] = SERVICE_GRANTED;
-    } else {
-        bzero(buf, MAXLINE);
-        buf[0] = SERVICE_REFUSED;
-
-        switch(k) {
-            case 1:
-                memcpy(buf+1, "Invalid Ticket", sizeof("Invalid Ticket"));
-                break;
-            case 2:
-                memcpy(buf+1, "Invalid IP address", sizeof("Invalid IP address"));
-                break;
-            case 3:
-                memcpy(buf+1, "Ticket has expired", sizeof("Ticket has expired"));
-                break;
-            case 4:
-                memcpy(buf+1, "Invalid data format", sizeof("Invalid data format"));
-                break;
-        }
     }
-    sendto(udpfd, buf, n, 0, (struct sockaddr *) &cliaddr, len);
+    else {
+        prepareRefuseBuffer(buf, k);
+    }
+    sendto(fd, buf, n, 0, (struct sockaddr *) &cliaddr, len);
 }
 
-void UDPTime(int &udpfd, char* buf) {
+void UDPTime(int fd, char* buf) {
     int n, k;
     struct sockaddr_in cliaddr;
     socklen_t len = sizeof(cliaddr);
 
-    n = recvfrom(udpfd, buf, MAXLINE, 0,(struct sockaddr *) &cliaddr, &len);
-    bzero(buf, MAXLINE);
+    n = recvfrom(fd, buf, BUFFER_SIZE, 0,(struct sockaddr *) &cliaddr, &len);
 
     if((k = TicketCorrectnessTester::CheckTicket(buf)) == 0) {
+        bzero(buf, BUFFER_SIZE);
         buf[0] = SERVICE_GRANTED;
-
         //Sends a time in seconds since the Epoch
         std::time_t result = std::time(nullptr);
         memcpy(buf+1, &result, sizeof(&result));
-    } else {
-        buf[0] = SERVICE_REFUSED;
-
-        switch(k) {
-            case 1:
-                memcpy(buf+1, "Invalid Ticket", sizeof("Invalid Ticket"));
-                break;
-            case 2:
-                memcpy(buf+1, "Invalid IP address", sizeof("Invalid IP address"));
-                break;
-            case 3:
-                memcpy(buf+1, "Ticket has expired", sizeof("Ticket has expired"));
-                break;
-            case 4:
-                memcpy(buf+1, "Invalid data format", sizeof("Invalid data format"));
-                break;
-        }
     }
-    sendto(udpfd, buf, n, 0, (struct sockaddr *) &cliaddr, len);
+    else {
+        prepareRefuseBuffer(buf, k);
+    }
+    sendto(fd, buf, n, 0, (struct sockaddr *) &cliaddr, len);
+}
+
+void TCPEcho(int fd, char* buf) {
+    int connfd, k;
+    int endPos;
+    struct sockaddr_in cliaddr;
+    socklen_t len = sizeof(cliaddr);
+    int rval, charsread;
+
+    if ((connfd = accept(fd, (struct sockaddr *) &cliaddr, &len)) == -1)
+        perror("accept");
+
+    if (fork() == 0) {
+        /* child process */
+        close(fd);
+        bzero(buf, BUFFER_SIZE);
+
+        if ((rval = read(connfd, buf, BUFFER_SIZE)) == -1)
+            perror("Reading stream message");
+        if((k = TicketCorrectnessTester::CheckTicket(buf)) == 0) {
+            buf[0] = SERVICE_GRANTED;
+        }
+        else {
+            prepareRefuseBuffer(buf, k);
+            write(connfd, buf, strlen(buf));
+            _exit(0);
+        }
+        if((endPos = checkIfEnd(buf,"END")) != std::string::npos) {
+            write(connfd, buf, endPos);
+        }
+        else {
+            FILE* pFile;
+            char fileName[64];
+            sprintf(fileName, "%d", getpid());
+            pFile = fopen(fileName, "w+");
+
+            fwrite(buf, sizeof(char), strlen(buf), pFile);
+            do {
+                bzero(buf, sizeof buf);
+                if ((rval = read(connfd, buf, BUFFER_SIZE)) == -1) {
+                    perror("Reading stream message");
+
+                    fclose(pFile);
+                    remove(fileName);
+
+                    _exit(0);
+                }
+                if(rval == 0) {
+                    printf("Premature end of echo connection. Closing service.\n");
+
+                    fclose(pFile);
+                    remove(fileName);
+
+                    _exit(0);
+                }
+                else if((endPos = checkIfEnd(buf,"END")) != std::string::npos) {
+                    fwrite(buf, sizeof(char), endPos, pFile);
+                    break;
+                }
+                else
+                    fwrite(buf, sizeof(char), strlen(buf), pFile);
+            }while(true);
+
+            rewind(pFile);
+            bzero(buf, sizeof buf);
+
+            do{
+                charsread = fread(buf, sizeof(char), BUFFER_SIZE, pFile);
+                write(connfd, buf, charsread);
+            }while(charsread == 1024);
+
+            fclose(pFile);
+            remove(fileName);
+        }
+        _exit(0);
+    }
+    close(connfd);
+}
+
+void TCPTime(int fd, char* buf) {
+    int connfd, k;
+    int endPos;
+    struct sockaddr_in cliaddr;
+    socklen_t len = sizeof(cliaddr);
+    int rval, charsread;
+
+    if ((connfd = accept(fd, (struct sockaddr *) &cliaddr, &len)) == -1) {
+        perror("accept");
+        return;
+    }
+
+    bzero(buf, BUFFER_SIZE);
+
+    if ((rval = read(connfd, buf, BUFFER_SIZE)) == -1) {
+        perror("Reading stream message");
+        close(connfd);
+        return;
+    }
+    if((k = TicketCorrectnessTester::CheckTicket(buf)) == 0) {
+        bzero(buf, BUFFER_SIZE);
+        buf[0] = SERVICE_GRANTED;
+        std::time_t result = std::time(nullptr);
+        memcpy(buf+1, &result, sizeof(&result));
+    }
+    else {
+        prepareRefuseBuffer(buf, k);
+    }
+    write(connfd, buf, strlen(buf));
+    close(connfd);
+}
+
+int checkIfEnd(char const* buf, char const* seq) {
+    std::string bufs(buf);
+    std::string subs(seq);
+
+    auto pos = bufs.find(seq);
+    return pos;
+}
+
+void prepareRefuseBuffer(char * buf, int errNum) {
+    bzero(buf, BUFFER_SIZE);
+    buf[0] = SERVICE_REFUSED;
+    switch(errNum) {
+        case 1:
+            memcpy(buf+1, "Invalid Ticket", sizeof("Invalid Ticket"));
+            break;
+        case 2:
+            memcpy(buf+1, "Invalid IP address", sizeof("Invalid IP address"));
+            break;
+        case 3:
+            memcpy(buf+1, "Ticket has expired", sizeof("Ticket has expired"));
+            break;
+        case 4:
+            memcpy(buf+1, "Invalid data format", sizeof("Invalid data format"));
+            break;
+    }
 }
